99fad6f46496eed83a903c60cdf0b3fda54d7a62
ytrytio <ghostersh.yt@gmail.com>
2025-09-04 12:41:04 +0200

    oops! x2

--- bitcrawler/signals.py
+++ bitcrawler/signals.py
@@ -1,9 +1,15 @@
 import asyncio
-import sys
 import os
+from bitcrawler.config import PROJECT_DIR
 
 restart_event = asyncio.Event()
 
+
 async def restart_process():
     await asyncio.sleep(2)
-    os.execvpe("uv", ["uv", "run", "python", "-m", "bitcrawler"], os.environ)
+    os.chdir(PROJECT_DIR)
+    os.execvpe(
+        "uv",
+        ["uv", "run", "python", "-m", "bitcrawler"],
+        os.environ
+    )
