b6e25ad6a35e6a16d410efde15cee25eaa03646a
ytrytio <ghostersh.yt@gmail.com>
2025-09-04 13:22:02 +0200

    Update ping

--- bitcrawler/commands/ping.py
+++ bitcrawler/commands/ping.py
@@ -1,28 +1,17 @@
-from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import Message
 from bitcrawler.utils.utils import bq
 
 async def ping(message: Message):
     user_date = message.date
     ping_msg = await message.reply(
         bq("Пинг..."),
-        parse_mode="HTML",
-        reply_markup=InlineKeyboardMarkup(
-            inline_keyboard=[
-                [InlineKeyboardButton(text="Главное меню", callback_data="menu")]
-            ]
-        )
+        parse_mode="HTML"
     )
     bot_date = ping_msg.date
-    delta_seconds = (bot_date - user_date).total_seconds()
-    delta_ms = int(delta_seconds * 1000)
+    delta_ms = (bot_date - user_date).total_seconds() * 1000
 
     await ping_msg.edit_text(
         bq("Пинг... ") + bq("Понг!") + "\n" +
-        bq("Задержка:", f"{delta_ms}ms"),
-        parse_mode="HTML",
-        reply_markup=InlineKeyboardMarkup(
-            inline_keyboard=[
-                [InlineKeyboardButton(text="Главное меню", callback_data="menu")]
-            ]
-        )
+        bq("Задержка:", f"{delta_ms:.1f}ms"),
+        parse_mode="HTML"
     )
